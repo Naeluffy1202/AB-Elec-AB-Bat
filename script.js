57ee8b799af5c495e460fcc1dc51a56a9847c843
Nael <nael1202@icloud.com>
2025-06-22 21:30:43 +0200

    ajout 3eme onglets Projes (Slider)

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ document.addEventListener("DOMContentLoaded", function () {
     navLinks.classList.toggle("show");
   });
 });
+
+const slides = document.getElementById('slides');
+  const totalSlides = slides.children.length;
+  let index = 0;
+
+  setInterval(() => {
+    index = (index + 1) % totalSlides;
+    slides.style.transform = `translateX(-${index * 100}%)`;
+  }, 3000);
